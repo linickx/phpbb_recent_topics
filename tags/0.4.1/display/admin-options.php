d26745055db0a754bd268576e4a93801f9ae4d90
linickx <linickx@gmail.com>
2008-12-01 11:22:47 +0000

    Uploading Version 0.4.1
    git-svn-id: http://plugins.svn.wordpress.org/phpbb-recent-topics@76713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.4.1/display/admin-options.php
@@ -0,0 +1,67 @@
+<?php
+
+# Where am I ?
+$location = get_option('siteurl') . '/wp-admin/admin.php?page=phpbb-recent-topics/display/admin-options.php';
+
+# Setup our phpbb Settings.
+add_option('prt_phpbb_db', __('', 'prt'));
+add_option('prt_phpbb_tt', __('phpbb_topics', 'prt'));
+add_option('prt_phpbb_url', __('', 'prt'));
+add_option('prt_phpbb_limit', __('5', 'prt'));
+add_option('prt_phpbb_date', __('d/M/y - g:i a', 'prt'));
+
+# If we've been submitted, then save :-)
+if ('process' == $_POST['stage'])
+{
+	update_option('prt_phpbb_db', $_POST['prt_phpbb_db']);
+	update_option('prt_phpbb_tt', $_POST['prt_phpbb_tt']);
+	update_option('prt_phpbb_url', $_POST['prt_phpbb_url']);
+	update_option('prt_phpbb_limit', $_POST['prt_phpbb_limit']);
+	update_option('prt_phpbb_date', $_POST['prt_phpbb_date']);
+}
+
+# When loading the form, fill in our old values....
+
+$prt_phpbb_db = stripslashes(get_option('prt_phpbb_db'));
+$prt_phpbb_tt = stripslashes(get_option('prt_phpbb_tt'));
+$prt_phpbb_url = stripslashes(get_option('prt_phpbb_url'));
+$prt_phpbb_limit = stripslashes(get_option('prt_phpbb_limit'));
+$prt_phpbb_date = stripslashes(get_option('prt_phpbb_date'));
+
+?>
+<div class="wrap">
+  <h2><?php _e('phpBB Recent Topics') ?></h2>
+  <form name="form1" method="post" action="<?php echo $location ?>&amp;updated=true">
+  <input type="hidden" name="stage" value="process" />
+  <table width="100%" cellspacing="2" cellpadding="5" class="editform">
+    <tr valign="top">
+      <th scope="row"><?php _e('phpbb MySQL Database Name') ?></th>
+      <td><input name="prt_phpbb_db" id="prt_phpbb_db" style="width: 80%;" rows="1" wrap="virtual" cols="50" value="<?php echo $prt_phpbb_db; ?>" />
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row"><?php _e('phpbb Topics Table Name') ?></th>
+      <td><input name="prt_phpbb_tt" id="prt_phpbb_tt" style="width: 80%;" rows="1" wrap="virtual" cols="50" value="<?php echo $prt_phpbb_tt; ?>" />
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row"><?php _e('phpbb forum URL') ?></th>
+      <td><input name="prt_phpbb_url" id="prt_phpbb_url" style="width: 80%;" rows="1" wrap="virtual" cols="50" value="<?php echo $prt_phpbb_url; ?>" />
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row"><?php _e('Number of Topics to show') ?></th>
+      <td><input name="prt_phpbb_limit" id="prt_phpbb_limit" style="width: 80%;" rows="1" wrap="virtual" cols="50" value="<?php echo $prt_phpbb_limit; ?>" />
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row"><?php _e('Date Formmating') ?></th>
+      <td><input name="prt_phpbb_date" id="prt_phpbb_date" style="width: 80%;" rows="1" wrap="virtual" cols="50" value="<?php echo $prt_phpbb_date; ?>" />
+      </td>
+    </tr>
+  </table>
+    <p class="submit">
+      <input type="submit" name="Submit" value="<?php _e('Update Options', 'prt') ?> &raquo;" />
+    </p>
+  </form>
+</div>
